5ee53556a48ed850c9846a54a5f450bfdcb9f32c
lazaronazareno <jonathan.levrero@gmail.com>
2020-01-10 16:15:02 -0300

    ejercicio tecnico 1

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,5 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+var parrafos = document.querySelectorAll('.comment--text');
+parrafos[0].style.color="red";
+parrafos[0].style.backgroundColor="black";
+parrafos[0].style.fontSize="150%";
